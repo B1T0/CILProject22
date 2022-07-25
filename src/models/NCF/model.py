581f8357d8a0657e0a50a41f5abbc5d67ff03ff4
Dustin Klebe <klebed@ethz.ch>
2022-07-25 16:09:00 +0200

    cuda add

--- src/models/NCF/model.py
+++ src/models/NCF/model.py
@@ -29,7 +29,6 @@ dic_test = {
 }
 
 
-print('Normalizing the data...')
 n_users = 10000
 n_items = 1000
 
@@ -38,8 +37,8 @@ print(np.asarray(dic_train['X']).shape, np.asarray(dic_train['y']).shape)
 train_dataset = TensorDataset(torch.Tensor(dic_train['X']), torch.Tensor(dic_train['y']))
 test_dataset = TensorDataset(torch.Tensor(dic_test['X']), torch.Tensor(dic_test['y']))
 
-train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=True)
+train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=5)
+test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=True, num_workers=5)
 
 
 # Define the model
@@ -82,13 +81,15 @@ class NCF(nn.Module):
 
 
 # Define the training loop
-def train(model, train_dataloader, test_dataloader, epochs=10, lr=0.001, weight_decay=0.0001):
+def train(model, train_dataloader, test_dataloader, epochs=10, lr=0.001, weight_decay=0.0001, early_stopping=True, patience=5):
     # Define the optimizer
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
     # Define the loss function
     loss_fn = torch.nn.CrossEntropyLoss()
     # Define the metrics
-    metrics = {'train_ce': [], 'test_ce': [], 'train_rmse': [], 'test_rmse': []}
+    metrics = {'train_ce': [], 'test_ce': [], 'train_rmse': [], 'test_rmse': [], 'train_rmse_epoch': [], 'test_rmse_epoch': []}
+    best_val_rsme = 100
+    best_epoch = 0
     # Train the model
     for epoch in range(epochs):
         print('Epoch {}/{}'.format(epoch+1, epochs))
@@ -99,7 +100,6 @@ def train(model, train_dataloader, test_dataloader, epochs=10, lr=0.001, weight_
             # Forward pass
             output = model(user_id, item_id)
             # Compute loss
-            # ratings_one_hot = one_hot_encode(rating)
             loss = loss_fn(output, rating.long()-1)
             # Backward pass
             optimizer.zero_grad()
@@ -107,13 +107,15 @@ def train(model, train_dataloader, test_dataloader, epochs=10, lr=0.001, weight_
             loss.backward()
             optimizer.step()
             # Update metrics
-            rmse_loss = rmse(softmax(output), rating.long())
+            rmse_loss = rmse(softmax(output), rating.long()).detach().numpy()
             metrics['train_ce'].append(loss.item())
             metrics['train_rmse'].append(rmse_loss)
+            metrics['train_rmse_epoch'].append(rmse_loss)
             if i % 5000 == 0:
                 print('\tBatch {}/{}'.format(i+1, len(train_dataloader)))
                 print('\t\tLoss: {}'.format(loss.item()))
-                print('\t\tRMSE: {}'.format(rmse_loss))
+                print('\t\tRMSE: {}'.format(np.mean(metrics['train_rmse_epoch'])))
+                metrics['train_rmse_epoch'] = []
         # Validation
         model.eval()
         with torch.no_grad():
@@ -122,25 +124,29 @@ def train(model, train_dataloader, test_dataloader, epochs=10, lr=0.001, weight_
                 # Forward pass
                 output = model(user_id, item_id)
                 # Compute loss
-                # ratings_one_hot = one_hot_encode(rating)
                 loss = loss_fn(output, rating.long()-1)
                 # Update metrics
-                rmse_loss = rmse(softmax(output), rating.long())
+                rmse_loss = rmse(softmax(output), rating.long()).detach().numpy()
                 metrics['test_ce'].append(loss.item())
                 metrics['test_rmse'].append(rmse_loss)
+                metrics['test_rmse_epoch'].append(rmse_loss)
                 if i % 5000 == 0:
                     print('\tBatch {}/{}'.format(i+1, len(test_dataloader)))
                     print('\t\tLoss: {}'.format(loss.item()))
-                    print('\t\tRMSE: {}'.format(rmse_loss))
+                    print('\t\tRMSE: {}'.format(np.mean(metrics['test_rmse_epoch'])))
+                    metrics['test_rmse_epoch'] = []
         # Print metrics
         print('Epoch: {}, Train CE: {:.4f}, Test CE: {:.4f}, Train_RSME: {:.4f}, Test_RMSE: {:.4f}'.format(epoch, np.mean(metrics['train_ce']), np.mean(metrics['test_ce']), np.mean(metrics['train_rmse']), np.mean(metrics['test_rmse'])))
+        # Keep track of the best model and do
+        if best_val_rsme > np.mean(metrics['test_rmse']):
+            best_val_rsme = np.mean(metrics['test_rmse'])
+            best_epoch = epoch
+            print('Best epoch: {}, Best RMSE: {:.4f}'.format(best_epoch, best_val_rsme))
+            torch.save(model.state_dict(), './best_model.pth')
+        if best_epoch <= epoch - patience:
+            print('Early stopping')
+            break
     return metrics
-
-def one_hot_encode(nums, num_classes=5):
-    one_hots = torch.zeros(nums.shape[0], num_classes)
-    for i, num in enumerate(nums.long()):
-        one_hots[i, num-1] = 1
-    return one_hots
 
 def rmse(preds, true):
     # one hot to index
@@ -160,3 +166,30 @@ def predict(model, user_id, item_id):
 print('Training the model...')
 model = NCF()
 metrics = train(model, train_dataloader, test_dataloader, epochs=10, lr=0.001, weight_decay=0.0001)
+print('Training complete.')
+
+# Save the model
+torch.save(model.state_dict(), './model_afterTraining.pt')
+
+# # Load the model
+# model = NCF()
+# model.load_state_dict(torch.load('./best_model.pt'))
+
+# Predict
+print('Predicting...')
+
+sample_sub = pd.read_csv("/home/dustin/Documents/Study/Master2/CILProject22/data_raw/sampleSubmission.csv")
+
+to_predict = torch.Tensor([(int(str(x).partition("_")[0][1:]),int(str(x).partition("_")[2][1:])) for x in sample_sub['Id']])
+user_id, item_id = torch.transpose(to_predict, 0, 1)[0].long(), torch.transpose(to_predict, 0, 1)[1].long()
+output = model(user_id, item_id)
+
+output_scalars = torch.zeros(output.shape[0])
+for i, pred in enumerate(output):
+    for j, p in enumerate(pred):
+        output_scalars[i] += p * (j+1)
+
+output_scalars = output_scalars.detach().numpy()
+sample_sub.Prediction = output_scalars
+sample_sub.to_csv("/home/dustin/Documents/Study/Master2/CILProject22/data/NCF.csv", index=False)
+print('Done!')
